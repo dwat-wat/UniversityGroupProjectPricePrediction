b5ecec18eddc0cc3d0352d45179e32dc8b28b983
dwat-wat <58701676+dwat-wat@users.noreply.github.com>
2020-01-30 00:09:35 +0000

    Update
    Refactorede code

--- Scripts/Script.py
+++ Scripts/Script.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.svm import SVR, LinearSVR
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
+import warnings
 
 # Properties
 apikey = "57b9285261bf74807812697d0aa133456f7b0054c5fd040b232003cd4b22de3f"
@@ -16,29 +17,15 @@ prices = []
 # Classes
 
 # Functions
-def get_data_thismonth():
-    url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=GBP&limit=" + str(datetime.datetime.today().timetuple().tm_yday-1) + "&aggregate=1&api_key=" + apikey
-    response = requests.get(url)
-    if response.status_code == 200:
-        res = json.loads(response.content) 
-    
-        for data in res["Data"]["Data"]:
-            _date = datetime.datetime(1, 1, 1) + datetime.timedelta(seconds=data["time"])
-            _date = _date.replace(year=_date.year + 1969)
-            date = _date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-3]
-            print(_date, data["open"])
-            dates.append(_date.day)
-            prices.append(float(data["open"]))
-    else:
-        print("Error: ", response.status_code)
-    return
 
 def predict_prices(dates, prices, x):
     dates = np.reshape(dates, (len(dates), 1))
     svr_rbf = SVR(C=1e3, gamma = 0.1)
     svr_lin = LinearSVR(C=1e3, max_iter=1000)
     svr_rbf.fit(dates, prices)
-    svr_lin.fit(dates, prices)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        svr_lin.fit(dates, prices)
     
     plt.scatter(dates, prices, color='blue', label='Data')
     plt.plot(dates, svr_rbf.predict(dates), color='green', label='RBF Model')
@@ -55,14 +42,32 @@ def predict_prices(dates, prices, x):
     plt.show()    
     
     return prediction_rbf, prediction_lin
+
+# Monthly Data
+def get_data_thismonth(query):
+    url = "https://min-api.cryptocompare.com/data/v2/" + query + str(datetime.datetime.today().timetuple().tm_yday-1) + "&api_key=" + apikey
+    response = requests.get(url)
+    if response.status_code == 200:
+        res = json.loads(response.content) 
+    
+        for data in res["Data"]["Data"]:
+            _date = datetime.datetime(1, 1, 1) + datetime.timedelta(seconds=data["time"])
+            #_date = _date.replace(year=_date.year + 1969)
+            dates.append(_date.day)
+            prices.append(float(data["open"]))
+    else:
+        print("Error: ", response.status_code)
+    return
     
 def thismonth():
-    get_data_thismonth()
+    print("This Month:\n")
+    get_data_thismonth("histoday?fsym=BTC&tsym=GBP&limit=")
     predictedprices = predict_prices(dates, prices, (datetime.datetime.today()+datetime.timedelta(days=1)).day)
     print("Tomorrows price: ", "\nRBF £", predictedprices[0], "\nLinear £", predictedprices[1])
 
-def get_data_pastdays(ndays):
-    url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=GBP&limit=" + str(ndays) + "&aggregate=1&api_key=" + apikey
+# Daily Data
+def get_data_pastdays(query, ndays):
+    url = "https://min-api.cryptocompare.com/data/v2/" + query + str(ndays) + "&api_key=" + apikey
     response = requests.get(url)
     if response.status_code == 200:
         res = json.loads(response.content)     
@@ -71,21 +76,22 @@ def get_data_pastdays(ndays):
             dates.append(i)
             prices.append(float(data["open"]))
             i += 1
-            
-        print(i)
     else:
         print("Error: ", response.status_code)
     return
 
 def pastdays(ndays):
-    get_data_pastdays(ndays)
+    print("Past " + str(ndays) +" days:\n")
+    get_data_pastdays("histoday?fsym=BTC&tsym=GBP&limit=", ndays)
     predictedprices = predict_prices(dates, prices, ndays+1)
     print("Tomorrows price: ", "\nRBF £", predictedprices[0], "\nLinear £", predictedprices[1])
 
 # Main    
 
-#thismonth()
-pastdays(100)
+thismonth()
+dates = []
+prices = []
+pastdays(250)
 
 
 
